b687d7dceb0646dfdba8cd5c853288de950edce0
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2024-05-18 20:41:46 -0300

    Create MyReadingAPI && Created Book Model

---
+++ MyReadingAPI/MyReading.API/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyReading.API.Models
+{
+    public class Book
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public DateTime DateRead { get; set; }
+
+    }
+}
